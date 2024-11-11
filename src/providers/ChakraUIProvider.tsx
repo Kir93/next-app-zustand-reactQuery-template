a9aad798b86174d6ce5a8b91872cfb138cbd06db
Kir93 <kir931028@gmail.com>
2024-11-11 16:51:22 +0900

    update chakra ui v3 theme setting

--- src/providers/ChakraUIProvider.tsx
+++ src/providers/ChakraUIProvider.tsx
@@ -4,5 +4,5 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from '@styles/theme';
 
 export default function ChakraUIProvider({ children }: { children: React.ReactNode }) {
-  return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
+  return <ChakraProvider value={theme}>{children}</ChakraProvider>;
 }
