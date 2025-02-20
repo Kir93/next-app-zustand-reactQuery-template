6abcbf7d3b75acae406252a6d3b50877f2550fa2
Kir93 <kir931028@gmail.com>
2025-02-21 04:30:03 +0900

    update add useInput hook jsDoc

--- src/utils/useInput.ts
+++ src/utils/useInput.ts
@@ -1,5 +1,22 @@
 import { useCallback, useState } from 'react';
 
+/**
+ * 폼 입력값 상태를 관리하고 변경 핸들러를 제공하는 커스텀 훅
+ *
+ * @template T - 입력값의 타입
+ * @param initValue - 입력값의 초기값
+ * @returns 다음을 포함하는 튜플:
+ *  - 현재 입력값
+ *  - 입력 요소의 변경 이벤트 핸들러
+ *  - 값을 직접 업데이트하는 설정자 함수
+ *
+ * @example
+ * ```typescript
+ * const [value, handleChange, setValue] = useInput<string>("");
+ * // 입력 요소에서 사용:
+ * <input value={value} onChange={handleChange} />
+ * ```
+ */
 const useInput = <T>(
   initValue: T
 ): [
